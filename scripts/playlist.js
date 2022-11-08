31f42febc7f0a7e117f691cdc4f3dc568ee309be
oepop <oepop@gmail.com>
2022-11-09 02:33:09 +0800

    fix bug: cant play

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,15 +1,16 @@
-tracklist = 
+trackList = 
 [ 
-{name:"【Ado】逆光.mp3", src:"【Ado】逆光.mp3"},
-{name:"【ALI】LOST IN PARADISE.mp3", src:"【ALI】LOST IN PARADISE.mp3"},
-{name:"【優里】ドライフラワー.mp3", src:"【優里】ドライフラワー.mp3"},
-{name:"【YOASOBI】三原色.mp3", src:"【YOASOBI】三原色.mp3"},
-{name:"【Aimer】残響散歌.mp3", src:"【Aimer】残響散歌.mp3"},
-{name:"【King Gnu】 一途.mp3", src:"【King Gnu】 一途.mp3"},
-{name:"【SEKAI NO OWARI】Habit.mp3", src:"【SEKAI NO OWARI】Habit.mp3"},
-{name:"【ROSA WALTON & HALLIE COGGINS】 I REALLY WANT TO STAY AT YOUR HOUSE.mp3", src:"【ROSA WALTON & HALLIE COGGINS】 I REALLY WANT TO STAY AT YOUR HOUSE.mp3"},
-{name:"【Creepy Nuts】 よふかしのうた.mp3", src:"【Creepy Nuts】 よふかしのうた.mp3"},
-{name:"【Eve】いのちの食べ方.mp3", src:"【Eve】いのちの食べ方.mp3"},
-{name:"【魔界ノりりむ × 星川サラ】だいしきゅーだいしゅき.mp3", src:"【魔界ノりりむ × 星川サラ】だいしきゅーだいしゅき.mp3"},
-{name:"【結束バンド】「ギターと孤独と蒼い惑星」.mp3", src:"【結束バンド】「ギターと孤独と蒼い惑星」.mp3"}
+{name:"【Ado】逆光.mp3", src:"music/【Ado】逆光.mp3"},
+{name:"【ALI】LOST IN PARADISE.mp3", src:"music/【ALI】LOST IN PARADISE.mp3"},
+{name:"【優里】ドライフラワー.mp3", src:"music/【優里】ドライフラワー.mp3"},
+{name:"【YOASOBI】三原色.mp3", src:"music/【YOASOBI】三原色.mp3"},
+{name:"【Aimer】残響散歌.mp3", src:"music/【Aimer】残響散歌.mp3"},
+{name:"【King Gnu】 一途.mp3", src:"music/【King Gnu】 一途.mp3"},
+{name:"【SEKAI NO OWARI】Habit.mp3", src:"music/【SEKAI NO OWARI】Habit.mp3"},
+{name:"【ROSA WALTON & HALLIE COGGINS】 I REALLY WANT TO STAY AT YOUR HOUSE.mp3", src:"music/【ROSA WALTON & HALLIE COGGINS】 I REALLY WANT TO STAY AT YOUR HOUSE.mp3"},
+{name:"【Creepy Nuts】 よふかしのうた.mp3", src:"music/【Creepy Nuts】 よふかしのうた.mp3"},
+{name:"【Eve】いのちの食べ方.mp3", src:"music/【Eve】いのちの食べ方.mp3"},
+{name:"【魔界ノりりむ × 星川サラ】だいしきゅーだいしゅき.mp3", src:"music/【魔界ノりりむ × 星川サラ】だいしきゅーだいしゅき.mp3"},
+{name:"【結束バンド】「ギターと孤独と蒼い惑星」.mp3", src:"music/【結束バンド】「ギターと孤独と蒼い惑星」.mp3"}
 ]; 
+
