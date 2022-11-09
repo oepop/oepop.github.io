7e126d5a6043963aa70456120c27f85213aa5fc3
oepop <oepop@gmail.com>
2022-11-10 06:34:41 +0800

    add cover

--- getMusicName.py
+++ getMusicName.py
@@ -1,11 +1,11 @@
 import os
 
-with open('./scripts/playlist.js', 'w') as f:
+with open('./js/playlist.js', 'w') as f:
     f.write('trackList = \n')
     f.write('[ \n')
     idx = 1
     for music in os.listdir('./music'):
-        f.write(f'{{name:"{music}", src:"music/{music}"}}')
+        f.write(f'{{"icon":"cover/{music.replace("mp3", "png")}", "title":"{music}", "file":"music/{music}"}}')
         f.write(',\n') if music != os.listdir('music')[-1] else f.write('\n')
 
     f.write(']; \n')
